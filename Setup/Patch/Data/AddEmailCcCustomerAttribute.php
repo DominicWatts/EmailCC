dc6282de0ce474790d10fcbae810936938b73364
Dominic Watts <dominic@xigen.co.uk>
2020-10-04 00:50:14 +0100

    1.1.0 release, framework update

---
+++ Setup/Patch/Data/AddEmailCcCustomerAttribute.php
@@ -0,0 +1,133 @@
+<?php
+
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+ declare(strict_types=1);
+
+namespace Xigen\CC\Setup\Patch\Data;
+
+use Magento\Customer\Model\Customer;
+use Magento\Customer\Setup\CustomerSetup;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Eav\Model\Entity\Attribute\Set;
+use Magento\Eav\Model\Entity\Attribute\SetFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+
+class AddEmailCcCustomerAttribute implements DataPatchInterface, PatchRevertableInterface
+{
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var CustomerSetup
+     */
+    private $customerSetupFactory;
+    /**
+     * @var SetFactory
+     */
+    private $attributeSetFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CustomerSetupFactory $customerSetupFactory
+     * @param SetFactory $attributeSetFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CustomerSetupFactory $customerSetupFactory,
+        SetFactory $attributeSetFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->customerSetupFactory = $customerSetupFactory;
+        $this->attributeSetFactory = $attributeSetFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerEntity = $customerSetup->getEavConfig()->getEntityType(Customer::ENTITY);
+        $attributeSetId = $customerEntity->getDefaultAttributeSetId();
+        
+        /** @var $attributeSet Set */
+        $attributeSet = $this->attributeSetFactory->create();
+        $attributeGroupId = $attributeSet->getDefaultGroupId($attributeSetId);
+        
+        $customerSetup->addAttribute(
+            Customer::ENTITY,
+            'email_cc',
+            [
+                'type' => 'varchar',
+                'label' => 'Email CC',
+                'input' => 'text',
+                'source' => '',
+                'required' => false,
+                'visible' => true,
+                'position' => 500,
+                'system' => false,
+                'backend' => '',
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => true,
+                'is_filterable_in_grid' => true,
+                'is_searchable_in_grid' => false,
+                'note' => __("Comma separated"),
+            ]
+        );
+        
+        $attribute = $customerSetup->getEavConfig()->getAttribute(Customer::ENTITY, 'email_cc');
+        $attribute->addData([
+            'used_in_forms' => [
+                'adminhtml_customer',
+                'adminhtml_checkout',
+                'customer_account_create',
+                'customer_account_edit'
+            ]
+        ]);
+        $attribute->addData([
+            'attribute_set_id' => $attributeSetId,
+            'attribute_group_id' => $attributeGroupId
+        
+        ]);
+        $attribute->save();
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerSetup->removeAttribute(Customer::ENTITY, 'email_cc');
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+}
